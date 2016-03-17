ba3f87f2adef0779d8b5815f554ef530b1064daf
Jaka Hudoklin <jakahudoklin@gmail.com>
2016-03-17 01:51:36 +0100

    Use sh -c for running if commands are strings

--- nix/lib.nix
+++ nix/lib.nix
@@ -3,7 +3,7 @@
 with lib;
 
 rec {
-  mkCommand = cmd: if isString cmd then (splitString " " cmd) else cmd;
+  mkCommand = cmd: if isString cmd then ["sh" "-c" cmd] else cmd;
 
   mkContainer = container: {
     name = container.name;
