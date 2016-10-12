efc63ccec10b039fd88465f8dda4717785a4d39d
Jaka Hudoklin <jakahudoklin@gmail.com>
2016-10-12 20:21:21 +0200

    Add support for referencing other deployments
    fixes #5

--- nix/default.nix
+++ nix/default.nix
@@ -7,45 +7,52 @@ with import ./lib.nix { inherit (pkgs) lib; inherit pkgs; };
 let
   deployments = import configuration;
 
-  configForDeployment = deployment:
-    (evalModules {
-      modules = [./options.nix deployment];
-      args = { inherit pkgs; };
-    }).config;
+  # Evaluates deployment
+  evalDeployment = name: deployment: let
+    otherDeployments = filterAttrs (name2: _: name != name2) deployments;
+  in (evalModules {
+    modules = [./options.nix deployment];
+    args = {
+      inherit pkgs;
+      deployments = mapAttrs (
+        name: deployment: evalDeployment name deployment
+      ) otherDeployments;
+    };
+  });
 
-    result = mapAttrs (name: deployment: let
-      config = configForDeployment deployment;
-    in {
-      # Deployment options
-      options = {
-        namespace = config.kubernetes.namespace.name;
+  result = mapAttrs (name: deployment: let
+    config = (evalDeployment name deployment).config;
+  in {
+    # Deployment options
+    options = {
+      namespace = config.kubernetes.namespace.name;
+    };
+
+    # Deployment resources
+    resources = {
+      namespaces = {
+        ${config.kubernetes.namespace.name} = mkNamespace config.kubernetes.namespace;
       };
+      pods =
+        mapAttrs (name: pod: mkPod pod) config.kubernetes.pods;
+      replicationcontrollers =
+        mapAttrs (name: ctrl: mkController ctrl) config.kubernetes.controllers;
+      deployments =
+        mapAttrs (name: deployment: mkDeployment deployment) config.kubernetes.deployments;
+      services =
+        mapAttrs (name: service: mkService service) config.kubernetes.services;
+      pvc =
+        mapAttrs (name: pvc: mkPvc pvc) config.kubernetes.pvc;
+      secrets =
+        mapAttrs (name: secret: mkSecret secret) config.kubernetes.secrets;
+      ingress =
+        mapAttrs (name: ing: mkIngress ing) config.kubernetes.ingress;
+    };
 
-      # Deployment resources
-      resources = {
-        namespaces = {
-          ${config.kubernetes.namespace.name} = mkNamespace config.kubernetes.namespace;
-        };
-        pods =
-          mapAttrs (name: pod: mkPod pod) config.kubernetes.pods;
-        replicationcontrollers =
-          mapAttrs (name: ctrl: mkController ctrl) config.kubernetes.controllers;
-        deployments =
-          mapAttrs (name: deployment: mkDeployment deployment) config.kubernetes.deployments;
-        services =
-          mapAttrs (name: service: mkService service) config.kubernetes.services;
-        pvc =
-          mapAttrs (name: pvc: mkPvc pvc) config.kubernetes.pvc;
-        secrets =
-          mapAttrs (name: secret: mkSecret secret) config.kubernetes.secrets;
-        ingress =
-          mapAttrs (name: ing: mkIngress ing) config.kubernetes.ingress;
-      };
-
-      # Keep jobs separated from other resources, as they have to be explicitly
-      # started
-      jobs = mapAttrs (name: job: mkJob job) config.kubernetes.jobs;
-    }) deployments;
+    # Keep jobs separated from other resources, as they have to be explicitly
+    # started
+    jobs = mapAttrs (name: job: mkJob job) config.kubernetes.jobs;
+  }) deployments;
 in pkgs.stdenv.mkDerivation {
   name = "configurations";
   buildCommand = ''
--- test/test.nix
+++ test/test.nix
@@ -1,8 +1,20 @@
 {
-  staging = {
+  deployment1 = {
     kubernetes.controllers.test = {
       pod.containers.test = {
         image = "test2";
+      };
+    };
+
+    kubernetes.services.test.ports = [{port = 1000;}];
+  };
+
+  deployment2 = {deployments, ...}: let
+    deployment1 = deployments.deployment1.config;
+  in {
+    kubernetes.deployments.test = {
+      pod.containers.test = {
+        image = deployment1.kubernetes.controllers.test.pod.containers.test.image;
       };
     };
 
